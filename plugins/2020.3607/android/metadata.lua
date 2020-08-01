96e3aed3ecf2bbd549fe36a6a75bfa233ff5c912
Vlad Shcherban <vlad@coronalabs.com>
2020-08-01 20:35:46 +0300

    Migrating to AndroidX

---
+++ plugins/2020.3607/android/metadata.lua
@@ -0,0 +1,22 @@
+local metadata =
+{
+    plugin =
+    {
+        format = 'jar',
+        manifest = 
+        {
+            permissions = {},
+            usesPermissions = {},            
+            usesFeatures = {},
+            applicationChildElements = 
+            {
+            },
+        }
+    },
+    coronaManifest = {
+        dependencies = {
+        },
+    },
+}
+
+return metadata
